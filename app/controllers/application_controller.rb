eb41d742583950b8578c9e8c9ce4606bc81a5f77
David Peloso <david.g.peloso@gmail.com>
2025-05-07 23:25:46 -0400

    Recursive Comments Beta

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # The helper_method statement makes current_user and logged_in? available to Views
+  # In addition to being available to all controllers since they are defined in this parent class
+  helper_method :current_user, :logged_in?
+  def current_user
+    # The below does:
+    #   If @current_user has a value, use it.
+    #   Otherwise, assign it with the expression after the ||= operator
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # Recall the !! operator turns any object into a boolean
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+end
